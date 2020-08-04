573084ccaf000031f7864d40b5006b63709cb287
joffre <joffre.hermosilla@gmail.com>
2020-08-03 23:48:27 -0500

    Primer commit Angular

---
+++ clientes-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+
+
+  curso: string = 'Curso Spring 5 con Angular 7';
+  profesor: string = 'Joffre Hermosilla Salas'
+
+}
