500e1072e96b17cc754618b4d313ff14c9bae65a
joffre <joffre.hermosilla@gmail.com>
2020-08-04 23:44:04 -0500

    Segundo commit Angular

--- clientes-app/src/app/app.module.ts
+++ clientes-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent }from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
